1747a5906a0aeb5c7174d4ceedaf768fd9e233aa
Longtao Zhang <DragonBillow@outlook.com>
2023-05-20 14:19:32 +0800

    feat: first commit
    Signed-off-by: Longtao Zhang <DragonBillow@outlook.com>

---
+++ src/main.rs
@@ -0,0 +1,52 @@
+// use minijinja::{context, Environment};
+use regex::Regex;
+
+fn generate_html(file_path: &str) {
+    let file_path = std::path::Path::new(file_path);
+    let parent_path = Into::<String>::into(file_path.parent().unwrap().to_str().unwrap())
+        .replace("content", "public");
+    if let Err(e) = std::fs::create_dir_all(&parent_path) {
+        println!("创建 {} 时发生错误：{}", parent_path, e);
+        return;
+    }
+
+    std::process::Command::new("asciidoctor")
+        .arg(file_path)
+        .arg("-D")
+        .arg(parent_path)
+        .spawn()
+        .unwrap()
+        .wait()
+        .unwrap();
+    // println!("{:?}", parent_path);
+}
+
+fn handle_file(file_path: &std::path::Path) {
+    if !file_path.exists() {
+        eprintln!("文件 {} 不存在", file_path.display());
+        return;
+    }
+
+    let dir_path = file_path.parent().unwrap();
+
+    if file_path.ends_with("index.adoc") {
+        generate_html(file_path.to_str().unwrap());
+        let content = std::fs::read_to_string(file_path).unwrap();
+        let re = Regex::new(r"xref:(.*)\[.*\]").unwrap();
+        for item in re.captures_iter(&content) {
+            let file_name: String = item.get(1).unwrap().as_str().into();
+            // let file_name = file_name.replacen("/", "", 1);
+            let file_path: String = dir_path.join(file_name.as_str()).to_str().unwrap().into();
+            // println!("{}", file_path);
+            handle_file(std::path::Path::new(&file_path));
+        }
+    } else {
+        generate_html(file_path.to_str().unwrap());
+    }
+}
+
+fn main() {
+    let file_path = "content/index.adoc";
+
+    handle_file(std::path::Path::new(file_path));
+}
