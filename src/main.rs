1bab16b8e4d49045f19da519e1c70438779b7414
Longtao Zhang <DragonBillow@outlook.com>
2023-05-20 15:02:10 +0800

    feat: 添加 tokio 以加快构建
    Signed-off-by: Longtao Zhang <DragonBillow@outlook.com>

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
-// use minijinja::{context, Environment};
 use regex::Regex;
 use tracing::*;
 use tracing_subscriber;
 
-fn generate_html(file_path: &str) {
-    let file_path = std::path::Path::new(file_path);
+async fn generate_html(file_path: String) {
+    let file_path = std::path::Path::new(&file_path);
     let parent_path = Into::<String>::into(file_path.parent().unwrap().to_str().unwrap())
         .replace("content", "public");
     if let Err(e) = std::fs::create_dir_all(&parent_path) {
@@ -22,10 +21,10 @@ fn generate_html(file_path: &str) {
         .unwrap()
         .wait()
         .unwrap();
-    // println!("{:?}", parent_path);
 }
 
-fn handle_file(file_path: &std::path::Path) {
+fn handle_file(file_path: String) {
+    let file_path = std::path::Path::new(&file_path);
     info!("处理文件：{}", file_path.display());
     if !file_path.exists() {
         warn!("文件 {} 不存在", file_path.display());
@@ -35,24 +34,25 @@ fn handle_file(file_path: &std::path::Path) {
     let dir_path = file_path.parent().unwrap();
 
     if file_path.ends_with("index.adoc") {
-        generate_html(file_path.to_str().unwrap());
+        tokio::spawn(generate_html(file_path.to_str().unwrap().into()));
         let content = std::fs::read_to_string(file_path).unwrap();
         let re = Regex::new(r"xref:(.*)\[.*\]").unwrap();
         for item in re.captures_iter(&content) {
             let file_name: String = item.get(1).unwrap().as_str().into();
-            // let file_name = file_name.replacen("/", "", 1);
             let file_path: String = dir_path.join(file_name.as_str()).to_str().unwrap().into();
-            // println!("{}", file_path);
-            handle_file(std::path::Path::new(&file_path));
+            handle_file(file_path);
         }
     } else {
-        generate_html(file_path.to_str().unwrap());
+        tokio::spawn(generate_html(file_path.to_str().unwrap().into()));
     }
 }
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<(), ()> {
     tracing_subscriber::fmt::init();
     let file_path = "content/index.adoc";
 
-    handle_file(std::path::Path::new(file_path));
+    handle_file(file_path.into());
+
+    Ok(())
 }
