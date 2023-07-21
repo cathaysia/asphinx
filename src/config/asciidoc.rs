c7280752ccd9d603780f7ae41b4ce7fc66dd237d
Longtao Zhang <DragonBillow@outlook.com>
2023-07-21 22:38:28 +0800

    fix(config): 修复 toml 解析问题
    Signed-off-by: Longtao Zhang <DragonBillow@outlook.com>

--- src/config/asciidoc.rs
+++ src/config/asciidoc.rs
@@ -1,9 +1,10 @@
 use std::collections::HashMap;
 
 use serde::{Deserialize, Serialize};
+use toml::Value;
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct Asciidoc {
     pub extensions: Vec<String>,
-    pub attributes: HashMap<String, String>,
+    pub attributes: HashMap<String, Value>,
 }
