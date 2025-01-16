28d1d4045b7441b371c565f58acd4a00b0a4cac0
loongtao.zhang <loongtao.zhang@outlook.com>
2025-01-16 13:43:33 +0800

    feat(theme): add history
    Signed-off-by: loongtao.zhang <loongtao.zhang@outlook.com>

---
+++ theme/src/history.tsx
@@ -0,0 +1,79 @@
+import { Button } from './components/ui/button';
+
+import { Dialog, DialogContent, DialogTrigger } from '@/components/ui/dialog';
+import { useMemo, useState } from 'react';
+import useSwr from 'swr';
+import { ScrollArea } from './components/ui/scroll-area';
+import { Input } from './components/ui/input';
+import { useDebounce } from 'use-debounce';
+
+type CacheType = [string, [string, string, string | null]][];
+
+export function History() {
+  const [search, setSearch] = useState('');
+  const [debounce] = useDebounce(search, 200);
+  const { data } = useSwr<CacheType>(
+    '/cache.json',
+    (input: RequestInfo | URL, init?: RequestInit) => {
+      return fetch(input, init).then(res => res.json());
+    },
+  );
+
+  const handledData = useMemo(() => {
+    if (data === undefined) {
+      return undefined;
+    }
+    const data1 = data.map(item => {
+      const [path, [_, title, time]] = item;
+      return {
+        path: path,
+        title: title,
+        time: time ? new Date(time) : null,
+      };
+    });
+    data1.sort((a, b) => {
+      if (!a.time) {
+        return 1;
+      }
+      if (!b.time) {
+        return -1;
+      }
+      return a.time > b.time ? -1 : 1;
+    });
+    return data1;
+  }, [data]);
+
+  return (
+    <Dialog>
+      <DialogTrigger>
+        <Button variant={'outline'}>History</Button>
+      </DialogTrigger>
+      <DialogContent className="flex w-full flex-col gap-2 overflow-y-scroll rounded-xl">
+        <Input
+          placeholder="type for search"
+          value={search}
+          onChange={e => setSearch(e.target.value)}
+        />
+        <ScrollArea className="h-[320px]">
+          <ul>
+            {handledData
+              ?.filter(item => {
+                return item.title.includes(debounce);
+              })
+              .map(item => {
+                const time = item.time?.toLocaleString();
+                return (
+                  <li key={item.path}>
+                    <a href={item.path} className="flex justify-between">
+                      <span>{item.title}</span>
+                      {time && <div>{`${time}`}</div>}
+                    </a>
+                  </li>
+                );
+              })}
+          </ul>
+        </ScrollArea>
+      </DialogContent>
+    </Dialog>
+  );
+}
